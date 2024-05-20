062a402c9f6600c28df468b65bae52a5bf5646bb
Max Rubel <maxrubel1@gmail.com>
2024-05-20 00:58:15 -0500

    nice

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+// @ts-nocheck
+
+  import Circle from "./Circle.svelte";
+
+
+let x= 0;
+let y= 0;
+let size = 0;
+let intervalId;
+let gameRun = false;
+
+const citcleMoves = () => {
+  if(gameRun){
+    size = 0
+    x = Math.floor(Math.random() * 800) + 1;
+    y = Math.floor(Math.random() * 1200) + 1;
+    intervalId = setInterval(() => {
+    size +=1
+  }, 60)
+  } else {
+      clearInterval(intervalId)
+  }
+}
+
+const buttonClick = () => {
+  gameRun = !gameRun
+
+  if(!gameRun){
+    clearInterval(intervalId);
+  } else {
+    citcleMoves();
+  }
+}
+
+const handleClick = () => {
+  citcleMoves()
+}
+
+</script>
+ 
+<main>
+  <button on:click={buttonClick}> 
+    {#if gameRun}
+    STOP
+    {:else}
+    START
+    {/if}
+  </button>
+  <div class="game-board">
+
+<Circle {handleClick}{x}{y}{size} display={gameRun ? 'block' : 'none'}/>
+  </div>
+</main>
+
+<style>
+.game-board{
+  position: relative;
+}
+</style>
---
+++ src/Circle.svelte
@@ -0,0 +1,24 @@
+<script>
+export let handleClick;
+export let x;
+export let y;
+export let size
+export let display
+</script>
+
+<main>
+    <button class="circle" style="height: {size}px; width: {size}px; left:{x}px; top: {y}px; display: {display};" on:click={handleClick}>
+    </button>
+</main>
+
+<style>
+    .circle{
+        position: absolute;
+        cursor: pointer;
+        display: block;
+        height: 150px;
+        width: 150px;
+        border-radius: 100%;
+        background-color: aquamarine;
+    }
+</style>
